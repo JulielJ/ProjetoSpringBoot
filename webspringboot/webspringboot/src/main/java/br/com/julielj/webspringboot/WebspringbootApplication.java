715b078781e40b406c280b141c8e019802e5cdda
JulielJ <julieljunior@hotmail.com>
2022-04-19 22:38:00 -0300

    Add files via upload

---
+++ webspringboot/webspringboot/src/main/java/br/com/julielj/webspringboot/WebspringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.julielj.webspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspringbootApplication.class, args);
+	}
+
+}
